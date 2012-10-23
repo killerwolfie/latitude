ed88d8499a0cf5b365629f0d220c24e250887395
Daniel Benoy <daniel@benoy.name>
2012-10-22 18:17:53 -0700

    Gave the prop class a better name

--- objects/item_prop.py
+++ objects/item_prop.py
@@ -1,6 +1,6 @@
 from game.gamesrc.latitude.objects.item import LatitudeItem
 
-class LatitudeProp(LatitudeItem):
+class LatitudeItemProp(LatitudeItem):
     """
     This type of item is an ornamental prop.  Its purpose is to lie around and look pretty.  Users could potentially edit everything about it, including renaming it.
     """
