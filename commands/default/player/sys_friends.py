10ab405813da9fbf37341ee5050605ff76ad63e2
Daniel Benoy <daniel@benoy.name>
2013-05-21 21:00:10 -0400

    Progressed on implementing @friends
    Adding, removing, and opting out now work.
    
    Friend request alerting still doesn't work.

--- commands/default/player/sys_friends.py
+++ commands/default/player/sys_friends.py
@@ -1,5 +1,5 @@
 from ev import default_cmds
-from src.utils import prettytable
+from game.gamesrc.latitude.utils.search import match_player, match_character
 
 class CmdSysFriends(default_cmds.MuxPlayerCommand):
     """
@@ -17,9 +17,11 @@ class CmdSysFriends(default_cmds.MuxPlayerCommand):
         opt-outs you may have in place.
 
       @friends add=<player/character>
-        Add a player to your friend list.  (You can specify the player or the
-        character name, but either way, the player is added to your friend
-        list)
+        Add a player to your friend list.
+        You can specify the player or the character name, but either way, the
+        player is added to your friend list.
+        In order for a friend to appear on your list, you must both add each
+        other.
 
       @friends del=<player/character>
         Remove a player from your friend list.
@@ -61,6 +63,8 @@ class CmdSysFriends(default_cmds.MuxPlayerCommand):
         self.msg("Invalid '%s' command.  See 'help %s' for usage" % (self.cmdstring, self.key))
 
     def cmd_online(self):
+        if not self.check_optout():
+            return
         online_friends = []
         for friend_char in sorted(self.caller.get_friend_characters(online_only=True), key=lambda char: char.key.lower()):
             online_friends.append('{c%s{n ({c%s{n)' % (friend_char.key, friend_char.db.owner))
@@ -70,34 +74,101 @@ class CmdSysFriends(default_cmds.MuxPlayerCommand):
             self.msg('None of your friends are currently online.')
 
     def cmd_list(self):
+        if not self.check_optout():
+            return
         friends = self.caller.get_friend_players()
         if not friends:
             self.msg('You have no friends :(')
         else:
             for friend in sorted(friends, key=lambda friend_player: str(friend_player).lower()):
-                self.msg('+ ' + (friend.sessions and '{c' or '{C') + friend.key)
                 # Produce a list of this friend's characters
                 friend_playable_characters = friend.get_playable_characters()
+                # List the characters, unless there is only one character, and it has the same name as the player.
                 if friend_playable_characters:
-                    friend_chars_online = []
-                    friend_chars_offline = []
-                    for char in friend_playable_characters:
-                        # Don't include characters if they have 'opted out' from the friend system
-                        if char.db.friends_optout:
-                            continue
-                        if char.sessid:
-                            friend_chars_online.append('{n  - {c' + char.key)
-                        else:
-                            friend_chars_offline.append('{n  - {C' + char.key)
-                    friend_chars_online.sort()
-                    friend_chars_offline.sort()
-                    self.msg("\n".join(friend_chars_online + friend_chars_offline))
+                        friend_chars_online = []
+                        friend_chars_offline = []
+                        for char in friend_playable_characters:
+                            # Don't include characters if they have 'opted out' from the friend system
+                            if char.db.friends_optout:
+                                continue
+                            if char.sessid:
+                                friend_chars_online.append('{n  - {c' + char.key)
+                            else:
+                                friend_chars_offline.append('{n  - {C' + char.key)
+                        friend_chars_online.sort()
+                        friend_chars_offline.sort()
+                # Output the resulting entry
+                self.msg('+ ' + (friend_chars_online and '{c' or '{C') + friend.key)
+                if friend_chars_online or friend_chars_offline:
+                    if not (len(friend_playable_characters) == 1 and friend_playable_characters[0].key.lower() == friend.key.lower()):
+                        self.msg("\n".join(friend_chars_online + friend_chars_offline))
+        for character in self.caller.get_playable_characters():
+            if character.db.friends_optout:
+                self.msg('{ROpt-out: %s' % character.key)
    
     def cmd_add(self, targetname):
-        self.msg('STUB: Add')
+        player = self.caller
+        target = match_player(targetname, exact=True)
+        if not target:
+            self.msg('Player not found.')
+            return
+        # Check if this player is already a friend
+        if target in player.get_friend_players():
+            self.msg('%s is already your friend.' % target.key)
+        # Add the player to the friend list if needed
+        if player in target.db.friends_list:
+            # Already in the target's friend list.  Mutual friendship begins!  Bunnies and rainbows.
+            player.db.friends_list.add(target)
+            self.msg('Added "%s" as a friend!' % target.key)
+        else:
+            # Alert the target user (only if this isn't already a 'friend request' state.)
+            if not target in player.db.friends_list:
+                self.msg('STUB: Alert other user.  Mail system?')
+            player.db.friends_list.add(target)
+            # Not already in the target's friend list, so let them know this is only a 'friend request' state.
+            self.msg('Friend request sent.  In order for this player to appear in your friend list, they will have to add you as well.  Use %s/del %s to cancel the request if needed.' % (self.key, targetname))
 
     def cmd_del(self, targetname):
-        self.msg('STUB: Del')
+        player = self.caller
+        target = match_player(targetname, exact=True)
+        if not target:
+            self.msg('Player not found.')
+            return
+        if target in player.get_friend_players():
+            if target in player.db.friends_list:
+                player.db.friends_list.remove(target)
+            if player in target.db.friends_list:
+                target.db.friends_list.remove(player)
+            self.msg("You're no longer friends with \"%s\" D:", target.key)
+        else:
+            if target in player.db.friends_list:
+                player.db.friends_list.remove(target)
+            self.msg('Cancelling any outstanding friend requests with "%s".', targetname)
 
     def cmd_optout(self, targetname):
-        self.msg('STUB: Optout')
+        if targetname[:1] == '!':
+            optout = False
+            target = match_character(targetname[1:], exact=True)
+        else:
+            optout = True
+            target = match_character(targetname, exact=True)
+        if not target:
+            self.msg('Player not found.')
+            return
+        if target.get_owner() != self.caller:
+            self.msg('"%s" is not one of your characters.' % (target.key))
+            return
+        target.db.friends_optout = optout
+        if optout:
+            self.msg('"%s" is now invisible to the friend system.' % (target.key))
+        else:
+            self.msg('"%s" has opted back into the friend system.' % (target.key))
+
+    def check_optout(self):
+        optout_characters = set([str(char) for char in self.caller.get_all_puppets() if char.db.friends_optout])
+        if optout_characters:
+            self.msg('{RSorry.  One or more of your currently connected characters are opting out of the friend system.  ({r%s{R)' % ", ".join(optout_characters))
+            self.msg('{RTo continue using the friend system, either disconnect those characters, or remove the "opt out" flag with:')
+            self.msg('{r  @friends optout=!<character>')
+            return False
+        return True
--- utils/search.py
+++ utils/search.py
@@ -5,7 +5,7 @@ from ev import utils
 from src.players.models import PlayerDB
 from src.objects.models import ObjectDB
 
-def match_player(name):
+def match_player(name, exact=False):
     """
     This function is used to permit partial matching for players for convenience when parsing player commands.
     The following rules are used, in priority order:
@@ -14,24 +14,38 @@ def match_player(name):
       3) If there is an unambiguous partial match with 'startswith', for either a player or character (with a legitimate owning player), and that player is online, return it.
 
     This configuration makes it impossible to reference a character which has a name which matches a player, but not its own.  This is because on Latitude, player and character names should never collide, and this sort of problem is to be expected if it does.  The 'get_owner()' routine should be checking to see if a character exists in this broken state, and returning None, making the system recognize it as an orphaned character.  Naturally, this requires the command which creates new player accounts to carefully check for existing character names!
+
+    Also, you can ambiguously match a character and a player, so long as every possible match is the same player.
     """
     # Check for exact player matches
     matching_players = [match.typeclass for match in PlayerDB.objects.filter(user__username__iexact=name)]
     if matching_players:
         return matching_players[0]
     # Check for exact character matches
-    matching_characters = [match.typeclass for match in ObjectDB.objects.filter(db_key__iexact=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character")]
-    if matching_characters:
-        for matching_character in matching_characters:
-            matching_player = matching_character.get_owner() # This should return None in the case of a duplicate character name, so we don't have to check for that.
-            if matching_player:
-                return matching_player
-    # Check for inexact matches
-    matching_players = [match.typeclass for match in PlayerDB.objects.filter(user__username__istartswith=name)]
-    matching_characters = [match.typeclass for match in ObjectDB.objects.filter(db_key__istartswith=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character")]
-    if len(matching_players + matching_characters) != 1:
-        # Ambiguous match
-        return None
+    matching_players = [match.typeclass.get_owner() for match in ObjectDB.objects.filter(db_key__iexact=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character") and match.typeclass.get_owner()]
     if matching_players:
         return matching_players[0]
-    return matching_characters[0].get_owner()
+    # Check for inexact matches
+    if not exact:
+        matching_players = [match.typeclass for match in PlayerDB.objects.filter(user__username__istartswith=name)]
+        matching_players += [match.typeclass.get_owner() for match in ObjectDB.objects.filter(db_key__istartswith=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character") and match.typeclass.get_owner()]
+        if len(set(matching_players)) == 1: # Results must be unambiguous
+            return matching_players[0]
+    # No matches
+    return None
+
+def match_character(name, exact=False):
+    """
+    Like match_player, only it only returns characters.
+    """
+    # Check for exact character matches
+    matching_characters = [match.typeclass for match in ObjectDB.objects.filter(db_key__iexact=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character") and match.typeclass.get_owner()]
+    if matching_characters:
+        return matching_characters[0]
+    # Check for inexact matches
+    if not exact:
+        matching_players = [match.typeclass for match in ObjectDB.objects.filter(db_key__istartswith=name) if utils.inherits_from(match.typeclass, "src.objects.objects.Character") and match.typeclass.get_owner()]
+        if len(set(matching_players)) == 1: # Results must be unambiguous
+            return matching_players[0]
+    # No matches
+    return None
