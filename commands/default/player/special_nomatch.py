d51287ac7523fd7d4c564c7c9c02b221239bdc08
Daniel Benoy <daniel@benoy.name>
2013-05-26 14:07:22 -0400

    Improved the 'no match' command code

--- commands/default/player/special_nomatch.py
+++ commands/default/player/special_nomatch.py
@@ -8,8 +8,35 @@ class CmdNoMatch(Command):
     auto_help = False
 
     def func(self):
-        command = self.args.split()[0]
-        if command[0] == '@':
-            self.caller.msg('"%s" command not found.  (Try "help" for a list of commands)' % command)
-        else:
-            self.caller.msg('You try to "%s" but it doesn\'t seem to work here.' % command)
+        cmdline = self.args
+        cmd = cmdline.split()[0]
+        caller = self.caller
+        sessid = self.sessid
+        # Check if we're OOC, or if we're explicitly typing a system command with @cmd, in which case, just tell them it's an invalid command.
+        if cmd[0] == '@' or not hasattr(caller, 'player'):
+            self.msg('"%s" command not found.  (Try "help" for a list of commands)' % cmd)
+            return
+        # Check for a direction match
+        for direction in [
+            ('north', 'north'),
+            ('south', 'south'),
+            ('east', 'east'),
+            ('west', 'west'),
+            ('up', 'up'),
+            ('down', 'down'),
+            ('in', 'in'),
+            ('out', 'out'),
+            ('northeast', 'northeast'),
+            ('northwest', 'northwest'),
+            ('southeast', 'southeast'),
+            ('southwest', 'southwest'),
+            ('ne', 'northeast'),
+            ('nw', 'northwest'),
+            ('se', 'southeast'),
+            ('sw', 'southwest'),
+        ]:
+            if direction[0].startswith(cmdline.lower()):
+                self.msg("You can't go %s from here." % direction[1])
+                return
+        # Unknown action.
+        self.msg('You try to "%s" but it doesn\'t seem to work here.' % cmd)
