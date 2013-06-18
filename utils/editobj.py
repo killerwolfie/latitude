4e24c769b1022500c51505d4724ef1feb1acf543
Daniel Benoy <daniel@benoy.name>
2013-06-18 15:04:38 -0400

    Added multiple choice options to EditObj

--- utils/editobj.py
+++ utils/editobj.py
@@ -33,7 +33,10 @@ DEFAULTS = {
     'SEP' : {
         'cols' : 1,
         'menutext' : None,
-    }
+    },
+    'ATTR_CHOICE' : {
+        # No Defaults
+    },
 }
 
 # TODO: Define a list of required attributes, and valid attributes and check at __init__ time
@@ -50,59 +53,70 @@ class CmdEditObj(lineeditor.CmdLineEditorVi):
     locks = "cmd:all()"
 
     def func(self):
-       if self.objedit.state_current_field:
-           # We're currently inside a field.  So pass commands to the editor.
-           if self.editor_cmdstring != None:
-               if self.editor_cmdstring == ':q':
-                   if self.editor_args:
-                       self.msg('{rThe ":q" command takes no arguments.')
-                       return
-                   if not self.editor.is_unchanged():
-                       self.msg('{rText has changed.  Use ":w" to save changes before quitting.')
-                       return
-                   self.objedit.close_field()
-               elif self.editor_cmdstring == ':q!':
-                   if self.editor_args:
-                       self.msg('{rThe ":q!" command takes no arguments.')
-                       return
-                   self.objedit.close_field()
-               elif self.editor_cmdstring == ':w':
-                   if self.editor_args:
-                       self.msg('{rThe ":w" command takes no arguments.')
-                       return
-                   self.objedit.save_field(close=False)
-               elif self.editor_cmdstring == ':wq':
-                   if self.editor_args:
-                       self.msg('{rThe ":wq" command takes no arguments.')
-                       return
-                   self.objedit.save_field(close=True)
-               else:
-                   return super(CmdEditObj, self).func() # Try the default commands
-           else:
-               return super(CmdEditObj, self).func() # No command, use default behavior.
-       elif self.objedit.state_asking_to_save:
-           if self.raw_string.lower() == 'y':
-               if self.objedit.commit_precheck():
-                   self.objedit.commit()
-                   self.objedit.quit()
-               else:
-                   self.msg('Do you still want to try committing your changes?  [Y/N/C]:')
-           elif self.raw_string.lower() == 'n':
-               self.objedit.quit()
-           elif self.raw_string.lower() == 'c':
-               self.objedit.state_asking_to_save = False
-               self.objedit.display_menu()
-           else:
-               self.msg('Please use "Y", "N", or "C" to make your selection.  Do you want to commit your changes?  [Y/N/C]:')
-       else:
-           # Catch requests to quit
-           if self.args.lower() == 'q':
-               self.msg('Do you want to commit your changes?  [Y/N/C]:')
-               self.objedit.state_asking_to_save = True
-               return
-           
-           # We're not in the editor, so process commands for the menu
-           self.objedit.open_field(self.args)
+        if self.objedit.state_editor_field:
+            # We're currently inside a field.  So pass commands to the editor.
+            if self.editor_cmdstring != None:
+                if self.editor_cmdstring == ':q':
+                    if self.editor_args:
+                        self.msg('{rThe ":q" command takes no arguments.')
+                        return
+                    if not self.editor.is_unchanged():
+                        self.msg('{rText has changed.  Use ":w" to save changes before quitting.')
+                        return
+                    self.objedit.close_field()
+                elif self.editor_cmdstring == ':q!':
+                    if self.editor_args:
+                        self.msg('{rThe ":q!" command takes no arguments.')
+                        return
+                    self.objedit.close_field()
+                elif self.editor_cmdstring == ':w':
+                    if self.editor_args:
+                        self.msg('{rThe ":w" command takes no arguments.')
+                        return
+                    self.objedit.save_editor_field(close=False)
+                elif self.editor_cmdstring == ':wq':
+                    if self.editor_args:
+                        self.msg('{rThe ":wq" command takes no arguments.')
+                        return
+                    self.objedit.save_editor_field(close=True)
+                else:
+                    return super(CmdEditObj, self).func() # Try the default commands
+            else:
+                return super(CmdEditObj, self).func() # No command, use default behavior.
+        elif self.objedit.state_choice_field:
+            # We're currently in a multiple choice field
+            if self.raw_string.isdigit():
+                selection = int(self.raw_string) - 1
+            else:
+                selection = None
+            if selection != None and selection >= 0 and selection < len(self.objedit.state_choice_field['options']):
+                self.objedit.save_choice_field(selection, close=True)
+            else:
+                self.msg("{RThat's not a valid selection.")
+                self.objedit.display_choices()
+        elif self.objedit.state_asking_to_save:
+            if self.raw_string.lower() == 'y':
+                if self.objedit.commit_precheck():
+                    self.objedit.commit()
+                    self.objedit.quit()
+                else:
+                    self.msg('Do you still want to try committing your changes?  [Y/N/C]:')
+            elif self.raw_string.lower() == 'n':
+                self.objedit.quit()
+            elif self.raw_string.lower() == 'c':
+                self.objedit.state_asking_to_save = False
+                self.objedit.display_menu()
+            else:
+                self.msg('Please use "Y", "N", or "C" to make your selection.  Do you want to commit your changes?  [Y/N/C]:')
+        else:
+            # Catch requests to quit
+            if self.args.lower() == 'q':
+                self.msg('Do you want to commit your changes?  [Y/N/C]:')
+                self.objedit.state_asking_to_save = True
+                return
+            
+            # We're not in the editor, so process commands for the menu
+            self.objedit.open_field(self.args)
 
 class CmdsetEditObj(CmdSet):
     """
@@ -138,7 +152,8 @@ class EditObj(object):
         self.confirm = confirm
         self.key = key
         # Initialize state
-        self.state_current_field = None
+        self.state_editor_field = None
+        self.state_choice_field = None
         self.state_asking_to_save = False
         self.edited_attr = {}
         self.edited_name = None
@@ -153,6 +168,14 @@ class EditObj(object):
                 for def_attr, def_val in DEFAULTS[field['type']].items():
                     if not def_attr in field:
                         field[def_attr] = def_val
+        # Populate edited_attr with defaults if needed
+        for field in fields:
+            if not field['type'] == 'ATTR' and not field['type'] == 'ATTR_CHOICE':
+                continue
+            if not 'default' in field:
+                continue
+            if not self.obj or not self.obj.get_attribute(field['attribute']):
+                self.edited_attr[field['attribute']] = field['default']
         # Create and attach the editor cmdset to the user
         cmdset = CmdsetEditObj()
         cmd_entry = CmdEditObj()
@@ -160,12 +183,17 @@ class EditObj(object):
         cmd_entry.editor = self.editor
         cmdset.add(cmd_entry)
         caller.cmdset.add(cmdset)
+        # Show the menu to the user
         self.display_menu()
 
     def msg(self, message, sessid=None):
         if sessid == None:
             sessid = self.sessid
         self.caller.msg(message, sessid=sessid)
+
+    def display_choices(self):
+        for option_index, option_value in enumerate(self.state_choice_field['options']):
+            self.msg('%d. %s' % (option_index + 1, option_value[0]))
 
     def display_menu(self):
         menu_string = ''
@@ -175,7 +203,9 @@ class EditObj(object):
         header.draw_string(5, 0, '{B[{y %s {B]' % (self.key))
         item_string = '{B[{c %s {B]' % (self.obj and self.obj.return_styled_name(self.caller) or '{g<New Object>')
         header.draw_string(73 - evennia_color_len(item_string), 0, item_string)
-        menu_string += header.evennia_export() + '\n\n'
+        menu_string += header.evennia_export() + '\n'
+        if len(self.fields) > 0 and self.fields[0]['type'] != 'SEP':
+            menu_string += '\n'
         # Render body
         current_max_columns = None
         current_column = 1
@@ -202,15 +232,31 @@ class EditObj(object):
                 else:
                     name_string = self.obj and self.obj.key or self.new_object_name
                 menu_string += evennia_color_left('{C%s {b%s' % (field['menutext'], name_string), 78 / current_max_columns, dots=True)
-            elif field['type'] == 'ATTR':
+            elif field['type'] == 'ATTR' or field['type'] == 'ATTR_CHOICE':
+                # Grab the currently set string
                 if field['attribute'] in self.edited_attr:
                     attr_string = self.edited_attr[field['attribute']]
+                    attr_exists = True
                 elif self.obj and self.obj.has_attribute(field['attribute']):
                     attr_string = self.obj.get_attribute(field['attribute'])
-                elif 'default' in field:
-                    attr_string = field['default']
+                    attr_exists = True
                 else:
-                    attr_string = '{B[Unset]'
+                    attr_string = None
+                    attr_exists = False
+                # Convert the string, if this is an ATTR_CHOICE, to the option string
+                if attr_exists and field['type'] == 'ATTR_CHOICE':
+                    match = False
+                    for key, val in field['options']:
+                        if val == attr_string:
+                            attr_string = key
+                            match = True
+                            break
+                    if not match:
+                        attr_string = '{R[Unknown]'
+                else:
+                    if not attr_string:
+                        attr_string = '{B[Unset]'
+                # Trim the string, and add the menu option
                 if '\n' in attr_string:
                     attr_string = attr_string.split('\n')[0]
                     attr_string += '...'
@@ -244,8 +290,6 @@ class EditObj(object):
                             self.editor.set_buffer(self.edited_attr[field['attribute']])
                         elif self.obj and self.obj.has_attribute(field['attribute']):
                             self.editor.set_buffer(self.obj.get_attribute(field['attribute']))
-                        elif 'default' in field:
-                            self.editor.set_buffer(field['default'])
                         else:
                             self.editor.set_buffer(None)
                     elif field['type'] == 'NAME':
@@ -253,35 +297,43 @@ class EditObj(object):
                             self.editor.set_buffer(self.edited_name)
                         else:
                             self.editor.set_buffer(self.obj and self.obj.key or self.new_object_name)
-                else:
-                    continue
-                self.state_current_field = field
-                self.msg('\nNow editing "%s"...' % field['desc'])
-                self.editor.display_buffer()
+                    self.state_editor_field = field
+                    self.msg('\nNow editing "%s"...' % field['desc'])
+                    self.editor.display_buffer()
+                elif field['type'] == 'ATTR_CHOICE':
+                    self.state_choice_field = field
+                    self.msg('\nPlease make a selection for "%s"...' % field['desc'])
+                    self.display_choices()
                 return
-        self.msg('{rThat seems to be an invalid menu selection.')
+        self.msg('{RThat seems to be an invalid menu selection.')
 
-    def save_field(self, close=False):
+    def save_choice_field(self, selection, close=False):
+        self.edited_attr[self.state_choice_field['attribute']] = self.state_choice_field['options'][selection][1]
+        # Close if requested
+        if close:
+            self.close_field()
+
+    def save_editor_field(self, close=False):
         # Process the buffer, if this field is a line editor type
-        if self.state_current_field['type'] == 'ATTR' or self.state_current_field['type'] == 'NAME':
+        if self.state_editor_field['type'] == 'ATTR' or self.state_editor_field['type'] == 'NAME':
             # Verify that they supplied kosher results
             new_val = self.editor.get_buffer()
-            if self.state_current_field['maxchars'] != None and len(new_val) > self.state_current_field['maxchars']:
+            if self.state_editor_field['maxchars'] != None and len(new_val) > self.state_editor_field['maxchars']:
                 self.msg('{rUnable to save: Too many characters')
                 return
-            if self.state_current_field['maxwords'] != None and len([ word for word in re.split(r'\s+', new_val) if word != '']) > self.state_current_field['maxwords']:
+            if self.state_editor_field['maxwords'] != None and len([ word for word in re.split(r'\s+', new_val) if word != '']) > self.state_editor_field['maxwords']:
                 self.msg('{rUnable to save: Too many words')
                 return
-            if self.state_current_field['maxlines'] != None and len(new_val.split('\n')) > self.state_current_field['maxlines']:
+            if self.state_editor_field['maxlines'] != None and len(new_val.split('\n')) > self.state_editor_field['maxlines']:
                 self.msg('{rUnable to save: Too many lines')
                 return
-            if self.state_current_field['invalid_regex'] != None and re.search(self.state_current_field['invalid_regex'], new_val):
+            if self.state_editor_field['invalid_regex'] != None and re.search(self.state_editor_field['invalid_regex'], new_val):
                 self.msg('{rUnable to save: Invalid characters detected.')
                 return
-            if self.state_current_field['type'] == 'NAME' and not new_val:
+            if self.state_editor_field['type'] == 'NAME' and not new_val:
                 self.msg("{rNames can't be blank")
                 return
-            if UNIQUE_CHARACTER_NAMES and self.state_current_field['type'] == 'NAME' and self.obj and isinstance(self.obj, Character):
+            if UNIQUE_CHARACTER_NAMES and self.state_editor_field['type'] == 'NAME' and self.obj and isinstance(self.obj, Character):
                 if new_val != self.obj.name: # Pass if they're trying to change the name of the object to what it already is
                     if [obj for obj in search_object(new_val) if isinstance(obj, Character)]:
                         self.msg('{rThat name is already taken.')
@@ -289,7 +341,7 @@ class EditObj(object):
                     if new_val != self.obj.player.name and search_player(new_val):
                         self.msg("{rThat name matches someone's login.")
                         return
-            if UNIQUE_PLAYER_NAMES and self.state_current_field['type'] == 'NAME' and self.obj and isinstance(self.obj, Player):
+            if UNIQUE_PLAYER_NAMES and self.state_editor_field['type'] == 'NAME' and self.obj and isinstance(self.obj, Player):
                 if new_val != self.obj.name: # Pass if they're trying to change the name of the object to what it already is
                     if search_player(new_val):
                         self.msg('{rThat name is already taken.')
@@ -298,9 +350,9 @@ class EditObj(object):
                         self.msg("{rThat name matches the name of someone's character.")
                         return
             # Perform the save
-            if self.state_current_field['type'] == 'ATTR':
-                self.edited_attr[self.state_current_field['attribute']] = new_val
-            elif self.state_current_field['type'] == 'NAME':
+            if self.state_editor_field['type'] == 'ATTR':
+                self.edited_attr[self.state_editor_field['attribute']] = new_val
+            elif self.state_editor_field['type'] == 'NAME':
                 self.edited_name = new_val
             # Set the buffer pristine (Matching what's saved)
             self.editor.set_pristine()
@@ -311,8 +363,12 @@ class EditObj(object):
             self.close_field()
 
     def close_field(self):
-        self.state_current_field = None
-        self.msg('... Editor closed.')
+        if self.state_editor_field:
+            self.state_editor_field = None
+            self.msg('... Editor closed.')
+        elif self.state_choice_field:
+            self.state_choice_field = None
+            self.msg('... Returning to menu.')
         self.display_menu()
 
     def commit_precheck(self):
