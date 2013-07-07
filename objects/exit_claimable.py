43efe7dddcc891e74b0428b97c2dff3694379110
Daniel Benoy <daniel@benoy.name>
2013-07-07 04:10:23 -0400

    Moved some items to the private Latitude repository

--- objects/exit_claimable.py
+++
@@ -1,5 +0,0 @@
-from game.gamesrc.latitude.objects.exit_deadbolt import ExitDeadbolt
-
-class ExitClaimable(ExitDeadbolt):
-    def at_object_creation(self):
-        super(ExitClaimable, self).at_object_creation()
